564a362299c1a2b94fcc2e2f695781b70a2f2e85
aulainformatica <aulainformatica@UBSCOA402-04.unbosque.edu.co>
2026-02-23 16:39:30 -0500

    medicamento

--- MiVeterinaria/src/co/edu/unbosque/model/Medicamento.java
+++ MiVeterinaria/src/co/edu/unbosque/model/Medicamento.java
@@ -4,71 +4,48 @@ public class Medicamento extends Producto{
 	
 	private String nombreComercial;
 	private String nombreCientifico;
+	private String fechaCaducidad;
 	private boolean estaDisponible;
 	private float dosis;
+	
 	
 	public Medicamento() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Medicamento(String nombreComercial, String nombreCientifico, boolean estaDisponible, float dosis) {
+	
+	public Medicamento(String nombreComercial, String nombreCientifico, String fechaCaducidad, boolean estaDisponible,
+			float dosis) {
 		super();
 		this.nombreComercial = nombreComercial;
 		this.nombreCientifico = nombreCientifico;
+		this.fechaCaducidad = fechaCaducidad;
 		this.estaDisponible = estaDisponible;
 		this.dosis = dosis;
 	}
 
+
 	public Medicamento(String marca, float precio, int idProducto, String nombreComercial, String nombreCientifico,
-			boolean estaDisponible, float dosis) {
+			String fechaCaducidad, boolean estaDisponible, float dosis) {
 		super(marca, precio, idProducto);
 		this.nombreComercial = nombreComercial;
 		this.nombreCientifico = nombreCientifico;
+		this.fechaCaducidad = fechaCaducidad;
 		this.estaDisponible = estaDisponible;
 		this.dosis = dosis;
 	}
+
 
 	public Medicamento(String marca, float precio, int idProducto) {
 		super(marca, precio, idProducto);
 		// TODO Auto-generated constructor stub
 	}
 
-	public String getNombreComercial() {
-		return nombreComercial;
-	}
-
-	public void setNombreComercial(String nombreComercial) {
-		this.nombreComercial = nombreComercial;
-	}
-
-	public String getNombreCientifico() {
-		return nombreCientifico;
-	}
-
-	public void setNombreCientifico(String nombreCientifico) {
-		this.nombreCientifico = nombreCientifico;
-	}
-
-	public boolean isEstaDisponible() {
-		return estaDisponible;
-	}
-
-	public void setEstaDisponible(boolean estaDisponible) {
-		this.estaDisponible = estaDisponible;
-	}
-
-	public float getDosis() {
-		return dosis;
-	}
-
-	public void setDosis(float dosis) {
-		this.dosis = dosis;
-	}
 
 	@Override
 	public String toString() {
 		return super.toString() + "==Medicamento== \nNombre Comercial: " + nombreComercial + "\nNombre Cientifico: " + nombreCientifico
-				+ "\nEstá Disponible?:" + estaDisponible + "\nDosis: " + dosis + "------------------";
+				+ "\nHay Disponibilidad?:" + estaDisponible + "\nDosis: " + dosis + "\nFecha Caducidad: " + fechaCaducidad +  "------------------";
 	}
 	
 	
